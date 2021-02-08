da847690913f843cac44a368ca9a72bb1e725b8f
Avery Pawelek <averycpawelek@gmail.com>
2021-02-08 11:38:45 -0600

    Removing accidentally duplicated directories

--- src/AuthService/utils/multi_dict.py
+++
@@ -1,9 +0,0 @@
-class MultiDict(dict):
-    """
-    Provides multidict-type functionality to be used with WTForms.
-    """
-    def getlist(self, key):
-        return self[key]
-
-    def __repr__(self):
-        return type(self).__name__ + '(' + dict.__repr__(self) + ')'
